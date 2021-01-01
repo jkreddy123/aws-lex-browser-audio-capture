5b8e513dc07612b553d60a3cbaade4ffa5a857ee
jkreddy123 <reddy.jayakrishna@gmail.com>
2021-01-01 20:57:04 +0530

    Create constants.js

---
+++ example/constants.js
@@ -0,0 +1,8 @@
+const API_KEY ='AIzaSrtrsdfSc28ycHgu6ndOSw';
+const GCPBUCKET ='dgdfsdh'
+const PROJECT_ID = "sasfs-12884662509"
+const PRIVATE_KEY_ID = "30dadf22222gty457567tkghlgftu01e23390fbe1f62"
+const PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkin-----END PRIVATE KEY-----\n"
+const CLIENT_EMAIL = "324222223423-compute@developer.gserviceaccount.com"
+const CLIENT_ID = "4534562222224586679879"
+const CLIENT_X509_CERT_URL = "https://www.googleapis.com/robot/v1/metadata/x509/45645756-compute%40developer.gserviceaccount.com"
